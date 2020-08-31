68d037db318fd1bb134e5726e5c63ce054fca9c2
Scott Hoisington <hoyzington@gmail.com>
2020-08-31 23:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   // el = document.getElementById('text');
   // el.textContent = 'This is really cool!';
-  document.getElementById('text').textContent = 'This is really cool!';
+  // document.getElementById('text').textContent = 'This is really cool!';
 });
